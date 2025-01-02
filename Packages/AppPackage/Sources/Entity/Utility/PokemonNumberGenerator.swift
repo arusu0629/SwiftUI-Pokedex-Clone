aeae4df321d273562dd978c257ba9ea0341f25bb
af_nakandakari_toru <nakandakari_toru@andfactory.co.jp>
2025-01-02 12:33:37 +0900

    AppPackage作成

---
+++ Packages/AppPackage/Sources/Entity/Utility/PokemonNumberGenerator.swift
@@ -0,0 +1,25 @@
+//
+//  PokemonNumberGenerator.swift
+//  AppPackage
+//
+//  Created by af_nakandakari_toru on 2025/01/02.
+//
+
+import Foundation
+
+enum PokemonNumberGenerator {
+
+    static func generate(from url: String) -> Int? {
+        guard !url.isEmpty,
+              let number = generate(from: url) else {
+            return nil
+        }
+        return number
+    }
+
+    private static func generate(fromPokemon url: String) -> Int? {
+        var removePrefix = url.replacingOccurrences(of: "https://pokeapi.co/api/v2/pokemon/", with: "")
+        removePrefix.removeLast()
+        return Int(removePrefix)
+    }
+}
